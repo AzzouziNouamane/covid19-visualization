d8847b08cbceb9929974512bb78999c5a84b20d3
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2021-02-14 16:55:59 +0100

    #7 - Day/Night mode and persisted locally (on page refresh)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,50 @@
 import React from "react";
 import Cases from "./Cases/cases";
 import Map from "./Map/Map"
+import "./App.css";
+import ThemeMode from "./ThemeMode/ThemeMode";
+import UseLocalStorage from "./Utils/LocalStorage/UseLocalStorage";
+import { useState, useEffect } from 'react';
+import ThemeContext, { themes } from "./Context/Theme/ThemeContext";
 
 const App = () => {
+  const [storageMode, setStorageMode] = UseLocalStorage('darkmode');
+  const [theme, setTheme] = useState();
+  const toggleTheme = () => {
+    if (theme === themes.dark) {
+      setTheme(themes.light);
+      setStorageMode(themes.light);
+    }
+    else {
+      setTheme(themes.dark);
+      setStorageMode(themes.dark);
+    }
+  }
+
+	useEffect(() => {
+		setTheme(storageMode);
+	}, [storageMode]); 
+
+/* 	const changeThemeContext = useCallback((newTheme) => {
+		setTheme(newTheme);
+		setStorageMode(newTheme);
+	}, [setStorageMode]);
+
+	useEffect(() => {
+		setTheme(storageMode);
+	}, [storageMode]); */
+
   return (
-    <div>
-      <Map></Map>
-      <Cases></Cases>
+    <div className = "App" style = {storageMode}>
+    <ThemeContext.Provider value = {storageMode}>
+        <Map></Map>
+        <Cases></Cases>
+        <ThemeMode
+        onChange={toggleTheme}
+        mode={storageMode}></ThemeMode>
+    </ThemeContext.Provider>
     </div>
+
   );
 };
 
