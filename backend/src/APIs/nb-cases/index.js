389525253352a56bfb5afa42fbf8848d35895263
raniafe <raniafekih@hotmail.com>
2021-02-18 22:25:59 +0100

    save data per day , save min max #17

--- backend/src/APIs/nb-cases/index.js
+++ backend/src/APIs/nb-cases/index.js
@@ -1,21 +1,41 @@
-
-
 import express from 'express';
 const router = express.Router();
-import { parse } from '../../util/csv-parse.js';
+import { CasesParsePerDay, CasesParsePerMonth } from '../../util/csv-parse-cases-number.js';
 import { Cases } from '../../models/casesModel.js';
+import {CasesPerDay} from "../../models/casesModelPerDay.js";
+import {MinMax} from "../../models/min-max-nb-case.js";
 
 
-router.post('/parse', (req, res) => {
+router.post('/parse/per/month', async (req, res) => {
     try {
-        parse();
+        await CasesParsePerMonth();
         res.json("parsed");
     }catch (e) {
         res.json({"error" : e})
     }
 });
 
-router.get('/', (req, res) => {
+router.post('/parse/per/day', async (req, res) => {
+    try {
+        await CasesParsePerDay();
+        res.json("parsed");
+    }catch (e) {
+        res.json({"error" : e})
+    }
+});
+
+router.get('/all', (req, res) => {
+    try {
+        Cases.find({}, function(err, cases) {
+            res.json(cases);
+        });
+
+    }catch (e) {
+        res.json({"error" : e})
+    }
+});
+
+router.get('/data/month', (req, res) => {
     try {
         Cases.find({date : req.body.date}, function(err, cases) {
             res.json(cases);
@@ -25,6 +45,32 @@ router.get('/', (req, res) => {
         res.json({"error" : e})
     }
 });
+
+router.get('/data/day', (req, res) => {
+    try {
+        let day_date = new Date(req.body.date);
+        console.log(day_date);
+        CasesPerDay.find({date : day_date}, function(err, cases) {
+            res.json(cases);
+        });
+    }catch (e) {
+        res.json({"error" : e})
+    }
+});
+
+
+router.get('/minmax', async (req, res) => {
+    try {
+        let minMax = await MinMax.find({});
+        res.json({"min": minMax[0].min, "max": minMax[0].max});
+    }catch (e) {
+        res.json({"error" : e})
+    }
+});
+
+
+
+
 
 
 export default router;
--- backend/src/models/casesModelPerDay.js
+++ backend/src/models/casesModelPerDay.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-const CasesNumbersSchema = new Schema({
+const CasesNumbersPerDaySchemas= new Schema({
     date: {type: String, required: true, max: 100},
     regions: [{
         regionId: {type: String, required: true},
         newCases: {type: Number, required: true}
     }
     ]
-
 });
 
-export const Cases = mongoose.model('casespemonth', CasesNumbersSchema);
+export const CasesPerDay = mongoose.model('CasesPerDay', CasesNumbersPerDaySchemas);
