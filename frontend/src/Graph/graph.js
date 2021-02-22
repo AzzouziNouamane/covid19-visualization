c1642d5d156bdae897829c1afc1e805949f5efda
raniafe <raniafekih@hotmail.com>
2021-02-22 22:10:16 +0100

    graph

--- frontend/src/Graph/graph.js
+++ frontend/src/Graph/graph.js
@@ -2,39 +2,44 @@ import Chart from "react-google-charts";
 import React, { useState, useEffect } from "react";
 import Charto from "./chart";
 
-const api_URL = "http://localhost:3001/nbcases/data/month/";
+const api_URL = "http://localhost:3001/";
 
 const Graph = ({regionId}) => {
     const [nb_cases, setCases] = useState({});
     const [nb_mental_cases, setMental] = useState({});
 
     const fetchData = async () => {
-        const response = await fetch(api_URL + "53");
+        const response_nb_cases = await fetch(api_URL +"nbcases/data/month/" + "53");
         let elt_cases = [[
             { type: 'date', label: 'Day' },
             'Number of cases ',
         ]];
-        let elt_mental =[[
-            { type: 'date', label: 'Day' },
-            'Number of mental health issues ( depression, anxiety, problems of sleep',
-        ]];
-        let data = await response.json();
-        console.log(data);
-
+        let data = await response_nb_cases.json();
         data.forEach(e=>{
-            let data_cases = [new Date(e[0], e[1]-1), e[2]];
-            let data_mental = [new Date(e[0], e[1]-1), e[3]];
-            elt_cases.push(data_cases);
-            elt_mental.push(data_mental)
+            let elt = [new Date(e[0], e[1]-1), e[2]];
+            elt_cases.push(elt)
         });
-        console.log(elt_cases);
-        setMental(elt_mental);
         setCases(elt_cases);
 
     };
 
+    const fetchDataMentalCases = async ()=>{
+        const response_nb_mental = await fetch(api_URL +"mentalHealth/data/month/" + "53");
+        let elt_mental =[[
+            { type: 'date', label: 'Day' },
+            'Number of mental health issues ( depression, anxiety, problems of sleep',
+        ]];
+        let data = await response_nb_mental.json();
+        data.forEach(e=>{
+            let elt = [new Date(e[0], e[1]-1), e[2]];
+           elt_mental.push(elt)
+        });
+        setMental(elt_mental);
+    };
+
     useEffect(async () => {
        await fetchData();
+       await fetchDataMentalCases();
 
     }, []);
     return (
