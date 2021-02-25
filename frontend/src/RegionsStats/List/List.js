3c6d8126e25d167b9deb9aacf38785e6f0ff30c9
raniafe <raniafekih@hotmail.com>
2021-02-25 11:25:52 +0100

    add redirection to graph in the list #27

--- frontend/src/RegionsStats/List/List.js
+++ frontend/src/RegionsStats/List/List.js
@@ -2,38 +2,45 @@ import { Table } from "reactstrap";
 import PropTypes from "prop-types";
 import React from "react";
 import './List.css';
+import {REGIONS} from "../Map/regions";
 
 
-export const List = ({ columns, regionsNewCasesData }) => (
-    <Table bordered hover striped id="customers">
-        <thead>
-        <tr>
-            {columns.map((col) => (
-                <th key={col}>{col}</th>
-            ))}
-        </tr>
-        </thead>
-        <tbody>
-        {regionsNewCasesData.map((d) => (
-            <tr key={d.id}>
+export const List = ({ columns, newCasesNow }) =>{
+    const navigate = (regionId)=>{
+        window.location.href = "http://localhost:3000/graph/"+ regionId
+    };
+    return  (
+
+        <Table bordered hover striped id="customers">
+            <thead>
+            <tr>
                 {columns.map((col) => (
-                    <td key={col + d.id} style={{ verticalAlign: "middle" }}>
-                        {d[col]}
-                    </td>
+                    <th key={col}>{col}</th>
                 ))}
             </tr>
-        ))}
-        </tbody>
-    </Table>
-);
+            </thead>
+            <tbody>
+            {newCasesNow.map((d) => (
+                <tr key={d.id} onClick={()=>navigate(d.regionId)}>
+                    {columns.map((col) => (
+                        <td key={col + d.id} style={{ verticalAlign: "middle" }}>
+                            {d[col]}
+                        </td>
+                    ))}
+                </tr>
+            ))}
+            </tbody>
+        </Table>
+    );
 
+}
 List.propTypes = {
     columns: PropTypes.arrayOf(PropTypes.string).isRequired,
-    regionsNewCasesData: PropTypes.arrayOf(PropTypes.shape())
+    newCasesNowData: PropTypes.arrayOf(PropTypes.shape())
 };
 
 List.defaultProps = {
-    regionsNewCasesData: []
+    newCasesNowData: []
 };
   
 export default List;
