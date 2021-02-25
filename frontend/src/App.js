883c8b539c79f53bab1ce0836e8ede3148ee2327
raniafe <raniafekih@hotmail.com>
2021-02-25 14:05:23 +0100

    FrontEnd : contact form + theme integration #35

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import RegionsStats from "./RegionsStats/RegionsStats"
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import {BrowserRouter, Link, Route, Switch, Redirect} from 'react-router-dom';
 import Authentication from './Authentication/Authentication';
 import "./App.css";
 import ThemeMode from "./ThemeMode/ThemeMode";
 import UseLocalStorage from "./Utils/LocalStorage/UseLocalStorage";
 import { useState, useEffect } from 'react';
 import ThemeContext, { themes } from "./Context/Theme/ThemeContext";
+import Contact from "./ContactForm/contact-form";
+import Button from "reactstrap/lib/Button";
 
 const App = () => {
   const [storageMode, setStorageMode] = UseLocalStorage('darkmode');
   const [theme, setTheme] = useState(themes.light);
+  const [contactVisible, setVisibility] = useState(true);
+
 
   const toggleTheme = () => {
     if (theme.isDark) {
@@ -21,7 +25,7 @@ const App = () => {
       setTheme(themes.dark);
       setStorageMode(themes.dark);
     }
-  }
+  };
 
     useEffect(() => {
       if (window.matchMedia('(prefers-color-scheme: light)').matches) {
@@ -32,6 +36,10 @@ const App = () => {
       }
     }, [storageMode]);
 
+    let changeVisibility = () =>{
+        setVisibility(false)
+    };
+
   return (
     <div className="App" style={theme}>
         <BrowserRouter>
@@ -41,7 +49,14 @@ const App = () => {
             </Route>
             <Route exact path='/authentication' render={ (props) => <ThemeContext.Provider value={theme}> <Authentication {...props} /> </ThemeContext.Provider> } />
             <Route exact path='/home' render={ (props) => <ThemeContext.Provider value={theme}> <RegionsStats {...props} /> </ThemeContext.Provider> } />
+            <Route exact path='/contact' render={ (props) => <ThemeContext.Provider value={theme}> <Contact {...props} /> </ThemeContext.Provider> } />
+
         </Switch>
+            { contactVisible  ?
+            <Link to="/contact">
+                <Button style={{ backgroundColor : theme.isDark? "white" : "black", color : theme.isDark? "black" : "white", }} onClick={changeVisibility}> Contact </Button>
+            </Link> :<div></div>}
+
         </BrowserRouter>
         <ThemeMode onChange={toggleTheme} mode={theme}/>
     </div>
---
+++ frontend/src/ContactForm/contact-form.js
@@ -0,0 +1,70 @@
+import React, {useContext, useEffect, useState} from "react";
+import './styled.scss'
+import axios from "axios";
+import {colors} from "@material-ui/core";
+import ThemeContext from "../Context/Theme/ThemeContext";
+
+
+const Contact = () => {
+    const [formData, setFormData] = useState({});
+
+    const updateInput = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+    };
+    const theme = useContext(ThemeContext);
+    const handleSubmit = event => {
+        event.preventDefault();
+
+            sendEmail();
+            setFormData({
+                name: '',
+                email: '',
+                message: ''
+            })
+
+
+    };
+    const sendEmail = () => {
+        axios.post(
+            'http://localhost:3001/contact',
+            formData
+        ).catch(error => {
+                console.log(error)
+            })
+    };
+
+    return (
+        <>
+            <h1 style={{ color : theme.isDark? "white" : "black"}}>Contact</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    onChange={updateInput}
+                    value={formData.name || ''}
+                />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    onChange={updateInput}
+                    value={formData.email || ''}
+                />
+                <textarea
+                    type="text"
+                    name="message"
+                    placeholder="Message"
+                    onChange={updateInput}
+                    value={formData.message || ''}
+                ></textarea>
+                <button style={{ backgroundColor : theme.isDark? "white" : "black", color : theme.isDark? "black" : "white" }} type="submit">Submit</button>
+            </form>
+        </>
+    )
+}
+
+export default Contact;
